406f104da006120346e30186a94bdb1a453f50a1
ZzinB <mysteleee7@gmail.com>
2024-04-03 17:54:25 +0900

    Test: insert 테스트

---
+++ todo/src/test/java/com/example/todo/mapper/TodoMapperTests.java
@@ -0,0 +1,31 @@
+package com.example.todo.mapper;
+
+import com.example.todo.domain.TodoVO;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+class TodoMapperTests {
+
+    @Autowired(required = false)
+    private TodoMapper todoMapper;
+
+    @Test
+    void testInsert(){
+        TodoVO todoVO = TodoVO.builder()
+                .title("테스트")
+                .dueDate(LocalDate.of(2024,04,02))
+                .writer("user 0")
+                .build();
+
+        todoMapper.insert(todoVO);
+    }
+}
