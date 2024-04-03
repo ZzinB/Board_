fb4b86fc0717f968cbcf215d127a94c786c0d01f
ZzinB <mysteleee7@gmail.com>
2024-04-03 17:26:37 +0900

    Add: dto <-> vo 변환

---
+++ todo/src/main/java/com/example/todo/config/ModelMapperConfig.java
@@ -0,0 +1,24 @@
+package com.example.todo.config;
+
+//ModelMapper 설정
+//빈 등록
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper getMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
+    }
+}
