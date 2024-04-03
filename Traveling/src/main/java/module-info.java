d4966e9fb408541d77c8d34e044700924edd5625
ZzinB <mysteleee7@gmail.com>
2024-04-03 16:46:06 +0900

    Settings: (Spring+SpringMVC+Mybatis)프로젝트 생성

--- Traveling/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.traveling {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.traveling to javafx.fxml;
-    exports com.example.traveling;
-}
