ac776f833426686f0800dd31f1d6f9919e5dd1c7
ZzinB <mysteleee7@gmail.com>
2024-04-03 17:27:37 +0900

    Add: dto

---
+++ todo/src/main/java/com/example/todo/dto/TodoDTO.java
@@ -0,0 +1,22 @@
+package com.example.todo.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@ToString
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoDTO {
+    private Long tno;
+
+    private String title;
+
+    private LocalDate dueDate;
+
+    private boolean finished;
+
+    private String writer;
+}
