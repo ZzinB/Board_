875af67eb1cf57519a478ef50498deed4f43a241
ZzinB <mysteleee7@gmail.com>
2024-04-03 18:14:13 +0900

    Feat: register 기능

--- todo/src/main/java/com/example/todo/controller/TodoController.java
+++ todo/src/main/java/com/example/todo/controller/TodoController.java
@@ -1,17 +1,26 @@
 package com.example.todo.controller;
 
 import com.example.todo.dto.TodoDTO;
+import com.example.todo.service.TodoService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/todo")
 @Log4j2
+@RequiredArgsConstructor
 public class TodoController {
+
+    private final TodoService todoService;
 
     @RequestMapping("/list")
     public void list(){
@@ -24,8 +33,17 @@ public class TodoController {
     }
 
     @PostMapping("/register")
-    public void registerPOST(TodoDTO todoDTO){
+    public String registerPOST(@Valid TodoDTO todoDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
         log.info("POST register");
+
+        if(bindingResult.hasErrors()){
+            log.info("has Error !!!!!");
+            redirectAttributes.addFlashAttribute("erros", bindingResult.getAllErrors());
+            return "redirect:/todo/register";
+        }
         log.info(todoDTO);
+
+        todoService.register(todoDTO);
+        return "redirect:/todo/list";
     }
 }
