8628a65980585b8338d346dcfbf2bcf606737aeb
ZzinB <mysteleee7@gmail.com>
2024-04-04 17:23:15 +0900

    Test: 페이지네이션 계산 테스트, 검색 테스트

--- todo/src/test/java/com/example/todo/mapper/TodoMapperTests.java
+++ todo/src/test/java/com/example/todo/mapper/TodoMapperTests.java
@@ -1,6 +1,7 @@
 package com.example.todo.mapper;
 
 import com.example.todo.domain.TodoVO;
+import com.example.todo.dto.PageRequestDTO;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,5 +41,33 @@ class TodoMapperTests {
     void testSelectOne(){
         TodoVO todoVO = todoMapper.selectOne(2L);
         log.info(todoVO);
+    }
+
+    @Test
+    void testSelectList(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        List<TodoVO> voList = todoMapper.selectList(pageRequestDTO);
+
+        voList.forEach(vo -> log.info(vo));
+    }
+
+    @Test
+    void testSelectSearch(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .types(new String[]{"t", "w"})
+                .keyword("테스트")
+                .build();
+
+        List<TodoVO> voList = todoMapper.selectList(pageRequestDTO);
+
+        voList.forEach(vo -> log.info(vo));
+
+        log.info(todoMapper.getCount(pageRequestDTO));
     }
 }
