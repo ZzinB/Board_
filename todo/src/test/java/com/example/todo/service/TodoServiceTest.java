382aae216dda5a09a9e104531a35529dab407b0d
ZzinB <mysteleee7@gmail.com>
2024-04-04 15:26:40 +0900

    Test: 전체 조회 테스트

--- todo/src/test/java/com/example/todo/service/TodoServiceTest.java
+++ todo/src/test/java/com/example/todo/service/TodoServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.todo.service;
 
+import com.example.todo.domain.TodoVO;
 import com.example.todo.dto.TodoDTO;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,11 +25,21 @@ class TodoServiceTest {
     @Test
     void testRegister(){
         TodoDTO todoDTO = TodoDTO.builder()
-                .title("테스트")
+                .title("테스트2")
                 .dueDate(LocalDate.now())
-                .writer("user1")
+                .writer("user2")
                 .build();
         todoService.register(todoDTO);
+    }
+
+    @Test
+    void testGetAll(){
+        //given
+        //when
+        List<TodoDTO> dtoList = todoService.getAll();
+
+        //then
+        log.info(dtoList);
     }
 
 }
