b72ea7ea537e62ef46b277d21e2ec314ba0cec12
ZzinB <mysteleee7@gmail.com>
2024-04-05 14:31:17 +0900

    Add: Entity

---
+++ Board/src/main/java/com/example/board/domain/Board.java
@@ -0,0 +1,25 @@
+package com.example.board.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
