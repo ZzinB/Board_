71c01dce043246e81d6a94b6a8e89b0be0747062
ZzinB <mysteleee7@gmail.com>
2024-04-05 17:08:31 +0900

    Add: @Valid 추가

--- Board/src/main/java/com/example/board/dto/BoardDTO.java
+++ Board/src/main/java/com/example/board/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package com.example.board.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +16,19 @@ import java.time.LocalDateTime;
 public class BoardDTO {
 
     private Long bno;
+
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
+
+    @NotEmpty
     private String content;
+
+    @NotEmpty
     private String writer;
+
     private LocalDateTime regDate;
+
     private LocalDateTime modDate;
 
 }
