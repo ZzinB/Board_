202e39a8dcc4b2e3ed762ffe60a838841001332a
ZzinB <mysteleee7@gmail.com>
2024-04-05 17:11:32 +0900

    Feat: 조회 처리 컨트롤

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -56,4 +56,11 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
+    @GetMapping("/read")
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
+        BoardDTO boardDTO = boardService.readOne(bno);
+        log.info(boardDTO);
+        model.addAttribute("dto", boardDTO);
+    }
+
 }
