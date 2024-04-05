04b72bc4ec5166cf14e269854f5963475f863aeb
ZzinB <mysteleee7@gmail.com>
2024-04-05 16:21:07 +0900

    Feat: register 기능

---
+++ src/main/java/com/example/board/service/BoardService.java
@@ -0,0 +1,7 @@
+package com.example.board.service;
+
+import com.example.board.dto.BoardDTO;
+
+public interface BoardService {
+    Long register(BoardDTO boardDTO);
+}
---
+++ src/main/java/com/example/board/service/BoardServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.board.service;
+
+import com.example.board.domain.Board;
+import com.example.board.dto.BoardDTO;
+import com.example.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+@Transactional
+public class BoardServiceImpl implements BoardService{
+    private final ModelMapper modelMapper;
+    private final BoardRepository boardRepository;
+
+    @Override
+    public Long register(BoardDTO boardDTO) {
+        Board board = modelMapper.map(boardDTO, Board.class);
+        Long bno = boardRepository.save(board).getBno();
+        return bno;
+    }
+}
