e4b6d29bcd5df8f7858aae77d83b353fc58093cc
ZzinB <mysteleee7@gmail.com>
2024-04-05 18:15:33 +0900

    Feat: 댓글 insert 기능

---
+++ src/main/java/com/example/board/domain/Reply.java
@@ -0,0 +1,24 @@
+package com.example.board.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "board")
+public class Reply extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board board;
+
+    private String replyText;
+
+    private String replyer;
+}
