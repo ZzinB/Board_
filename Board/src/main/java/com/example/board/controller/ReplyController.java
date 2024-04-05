dcbbf2670f92c1dde98da43d88ca0a49e3ce3b82
ZzinB <mysteleee7@gmail.com>
2024-04-05 17:47:41 +0900

    Add: 댓글 엔티티

---
+++ Board/src/main/java/com/example/board/controller/ReplyController.java
@@ -0,0 +1,39 @@
+package com.example.board.controller;
+
+import com.example.board.dto.ReplyDTO;
+import io.swagger.annotations.ApiOperation;
+import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/replies")
+@Log4j2
+public class ReplyController {
+    @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Long> register(@Valid @RequestBody ReplyDTO replyDTO,
+                                      BindingResult bindingResult) throws BindException {
+        log.info(replyDTO);
+
+        if(bindingResult.hasErrors()){
+            throw new BindException(bindingResult);
+        }
+
+        Map<String, Long> resultMap = new HashMap<>();
+
+        resultMap.put("rno", 111L);
+
+        return resultMap;
+    }
+}
