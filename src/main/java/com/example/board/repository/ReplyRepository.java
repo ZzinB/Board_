2d5170c7878844de3bd255db2dbd9608aaf03c9a
ZzinB <mysteleee7@gmail.com>
2024-04-08 17:49:24 +0900

    Fixed: @Param 문제 해결

--- src/main/java/com/example/board/repository/ReplyRepository.java
+++ src/main/java/com/example/board/repository/ReplyRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     @Query("select r from Reply r where r.board.bno = :bno")
-    Page<Reply> listOfBoard(Long bno, Pageable pageable);
+    Page<Reply> listOfBoard(@Param("bno") Long bno, Pageable pageable);
 }
