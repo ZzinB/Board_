113985df5332b701b2fd3a71a355975ec8f64d1c
ZzinB <mysteleee7@gmail.com>
2024-04-08 18:03:25 +0900

    Code: 오타 수정

--- Board/src/main/java/com/example/board/repository/search/BoardSearchImpl.java
+++ Board/src/main/java/com/example/board/repository/search/BoardSearchImpl.java
@@ -2,7 +2,10 @@ package com.example.board.repository.search;
 
 import com.example.board.domain.Board;
 import com.example.board.domain.QBoard;
+import com.example.board.domain.QReply;
+import com.example.board.dto.BoardListReplyCountDTO;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.data.domain.Page;
@@ -12,6 +15,9 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 import java.util.List;
 
+/**
+ * Querydsl 이용한 search
+ */
 public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardSearch {
     public BoardSearchImpl(){
         super(Board.class);
@@ -76,5 +82,53 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
         long count = query.fetchCount();
 
         return  new PageImpl<>(list, pageable, count);
+    }
+
+    @Override
+    public Page<BoardListReplyCountDTO> searchWithReplyCount(String[] types, String keyword, Pageable pageable) {
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+
+        JPQLQuery<Board> query = from(board);
+        query.leftJoin(reply).on(reply.board.eq(board));
+
+        query.groupBy(board);
+
+        //검색 조건과 키워드가 존재한다면
+        if((types != null && types.length > 0) && keyword != null){
+            // (
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+            for(String type: types){
+                switch (type){
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains(keyword));
+                        break;
+                }
+            }
+            query.where(booleanBuilder);
+        }
+        //bno > 0
+        query.where(board.bno.gt(0L));
+
+        //Projection을 이용한 JPQL -> DTO
+        JPQLQuery<BoardListReplyCountDTO> dtoQuery = query.select(Projections.bean(BoardListReplyCountDTO.class,
+                board.bno,
+                board.title,
+                board.writer,
+                board.regDate,
+                reply.count().as("replyCount")
+        ));
+
+        this.getQuerydsl().applyPagination(pageable, dtoQuery);
+        List<BoardListReplyCountDTO> dtoList = dtoQuery.fetch();
+        long count = dtoQuery.fetchCount();
+        return new PageImpl<>(dtoList, pageable, count);
     }
 }
