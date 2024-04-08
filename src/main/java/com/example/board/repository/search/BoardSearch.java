d7fa236bbca4a5d041c51637d52b0bea452f5879
ZzinB <mysteleee7@gmail.com>
2024-04-08 18:03:25 +0900

    Code: 오타 수정

--- src/main/java/com/example/board/repository/search/BoardSearch.java
+++ src/main/java/com/example/board/repository/search/BoardSearch.java
@@ -1,6 +1,7 @@
 package com.example.board.repository.search;
 
 import com.example.board.domain.Board;
+import com.example.board.dto.BoardListReplyCountDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -8,4 +9,6 @@ public interface BoardSearch {
     Page<Board> search1(Pageable pageable);
 
     Page<Board> searchAll(String[] types, String  keyword, Pageable pageable);
+
+    Page<BoardListReplyCountDTO> searchWithReplyCount(String[] types, String keyword, Pageable pageable);
 }
