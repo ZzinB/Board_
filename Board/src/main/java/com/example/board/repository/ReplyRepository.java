39d4fbd009c6392439b4364293bb6d884e140dd7
ZzinB <mysteleee7@gmail.com>
2024-04-08 17:43:17 +0900

    Add: 페이징 @Query

--- Board/src/main/java/com/example/board/repository/ReplyRepository.java
+++ Board/src/main/java/com/example/board/repository/ReplyRepository.java
@@ -1,7 +1,13 @@
 package com.example.board.repository;
 
 import com.example.board.domain.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    @Query("select r from Reply r where r.board.bno = :bno")
+    Page<Reply> listOfBoard(Long bno, Pageable pageable);
 }
