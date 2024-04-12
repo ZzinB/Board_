1e4d88a506c3e6ff2be0bfaaebda4d7672c90421
ZzinB <mysteleee7@gmail.com>
2024-04-12 15:51:02 +0900

    Feat: 섬네일용 파일 저장
    파일명 "s_"로 시작하도록

--- Board/src/main/java/com/example/board/controller/UpDownController.java
+++ Board/src/main/java/com/example/board/controller/UpDownController.java
@@ -3,12 +3,15 @@ package com.example.board.controller;
 import com.example.board.dto.upload.UploadFileDTO;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -35,6 +38,12 @@ public class UpDownController {
 
                 try {
                     multipartFile.transferTo(savePath);
+
+                    if(Files.probeContentType(savePath).startsWith("image")){
+                        File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalName);
+
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
+                    }
                 } catch (IOException e){
                     e.printStackTrace();
                 }
