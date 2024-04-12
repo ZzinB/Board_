5aceed4c8bdf9b013f32b839f3f69895a854a22f
ZzinB <mysteleee7@gmail.com>
2024-04-12 15:57:21 +0900

    Feat: 첨부파일 업로드

--- Board/src/main/java/com/example/board/controller/UpDownController.java
+++ Board/src/main/java/com/example/board/controller/UpDownController.java
@@ -1,6 +1,7 @@
 package com.example.board.controller;
 
 import com.example.board.dto.upload.UploadFileDTO;
+import com.example.board.dto.upload.UploadResultDTO;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import net.coobird.thumbnailator.Thumbnailator;
@@ -14,6 +15,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -25,21 +28,28 @@ public class UpDownController {
 
     @ApiOperation(value = "Upload POST", notes = "POST 방식으로 파일 등록")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String upload(UploadFileDTO uploadFileDTO){
+    public List<UploadResultDTO> upload(UploadFileDTO uploadFileDTO){
         log.info(uploadFileDTO);
 
         if(uploadFileDTO.getFiles() != null){
+            final List<UploadResultDTO> list = new ArrayList<>();
+
             uploadFileDTO.getFiles().forEach(multipartFile -> {
                 String originalName = multipartFile.getOriginalFilename();
                 log.info(originalName);
 
                 String uuid = UUID.randomUUID().toString();
+
                 Path savePath = Paths.get(uploadPath, uuid + "_" + originalName);
+
+                boolean image = false;
 
                 try {
                     multipartFile.transferTo(savePath);
 
                     if(Files.probeContentType(savePath).startsWith("image")){
+                        image = true;
+
                         File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalName);
 
                         Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
@@ -47,7 +57,14 @@ public class UpDownController {
                 } catch (IOException e){
                     e.printStackTrace();
                 }
+                list.add(UploadResultDTO.builder()
+                        .uuid(uuid)
+                        .fileName(originalName)
+                        .img(image)
+                        .build());
+
             });
+            return list;
         }
         return null;
     }
