8e80c7e35d71f4d92ad0c07295ce09f0d635e665
ZzinB <mysteleee7@gmail.com>
2024-04-12 14:29:33 +0900

    Add: 댓글 수정용 changeText 추가
    Reply 객체 안에서 수정가능하도록

--- Board/src/main/java/com/example/board/domain/Reply.java
+++ Board/src/main/java/com/example/board/domain/Reply.java
@@ -24,4 +24,8 @@ public class Reply extends BaseEntity{
     private String replyText;
 
     private String replyer;
+
+    public void changeText(String text){
+        this.replyText = text;
+    }
 }
