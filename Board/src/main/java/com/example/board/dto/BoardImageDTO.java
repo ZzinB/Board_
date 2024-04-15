b12e2a32cb10f80a8af43544416557edac388715
ZzinB <mysteleee7@gmail.com>
2024-04-15 17:33:29 +0900

    Feat: 페이징, 이미지, 카운트 한번에 처리

---
+++ Board/src/main/java/com/example/board/dto/BoardImageDTO.java
@@ -0,0 +1,22 @@
+package com.example.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * BoardImage 엔티티 처리를 위한 dto
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardImageDTO {
+    private String uuid;
+
+    private String fileName;
+
+    private int ord;
+}
