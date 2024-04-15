2f4d0de227fd419651de8f1b8c96dbd4d506a643
ZzinB <mysteleee7@gmail.com>
2024-04-15 17:35:55 +0900

    Feat: 화면목록 최종

--- Board/src/main/java/com/example/board/repository/search/BoardSearchImpl.java
+++ Board/src/main/java/com/example/board/repository/search/BoardSearchImpl.java
@@ -3,17 +3,20 @@ package com.example.board.repository.search;
 import com.example.board.domain.Board;
 import com.example.board.domain.QBoard;
 import com.example.board.domain.QReply;
+import com.example.board.dto.BoardImageDTO;
+import com.example.board.dto.BoardListAllDTO;
 import com.example.board.dto.BoardListReplyCountDTO;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
-import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Querydsl 이용한 search
@@ -133,22 +136,69 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
     }
 
     @Override
-    public Page<BoardListReplyCountDTO> searchWithAll(String[] types, String keyword, Pageable pageable) {
+    public Page<BoardListAllDTO> searchWithAll(String[] types, String keyword, Pageable pageable) {
         QBoard board = QBoard.board;
         QReply reply = QReply.reply;
 
         JPQLQuery<Board> boardJPQLQuery = from(board);
         boardJPQLQuery.leftJoin(reply).on(reply.board.eq(board));
 
+        //검색 조건과 키워드가 존재한다면
+        if((types != null && types.length > 0) && keyword != null) {
+            // (
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+            for (String type : types) {
+                switch (type) {
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains(keyword));
+                        break;
+                }
+            }
+            boardJPQLQuery.where(booleanBuilder);
+        }
+        boardJPQLQuery.groupBy(board);
+
         getQuerydsl().applyPagination(pageable, boardJPQLQuery);
+
+        JPQLQuery<Tuple> tupleList = boardJPQLQuery.select(board, reply.countDistinct());
 
         List<Board> boardList = boardJPQLQuery.fetch();
 
-        boardList.forEach(board1 -> {
-            System.out.println(board1.getBno());
-            System.out.println(board1.getImageSet());
-            System.out.println("--------------------");
-        });
-        return null;
-    }
+        List<BoardListAllDTO> dtoList = tupleList.stream().map(tuple -> {
+            Board board1 = (Board) tuple.get(board);
+            long replyCount = tuple.get(1, long.class);
+
+            BoardListAllDTO dto = BoardListAllDTO.builder()
+                    .bno(board1.getBno())
+                    .title(board1.getTitle())
+                    .writer(board1.getWriter())
+                    .regDate(board1.getRegDate())
+                    .replyCount(replyCount)
+                    .build();
+
+            // BoardImage를 BoardImageDTO 처리
+            List<BoardImageDTO> imageDTOS = board1.getImageSet().stream().sorted().map(boardImage -> BoardImageDTO.builder()
+                    .uuid(boardImage.getUuid())
+                    .fileName(boardImage.getFileName())
+                    .ord(boardImage.getOrd())
+                    .build()
+            ).collect(Collectors.toList());
+
+            dto.setBoardImages(imageDTOS);
+
+            return dto;
+            }).collect(Collectors.toList());
+
+        long totalCount = boardJPQLQuery.fetchCount();
+
+        return  new PageImpl<>(dtoList, pageable, totalCount);
+        }
+
 }
