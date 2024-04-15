c6fd83c0cbe2a7c5a9306fe051ae1175a3b726a3
ZzinB <mysteleee7@gmail.com>
2024-04-15 16:12:39 +0900

    Feat: 게시물과 첨부파일 삭제

--- Board/src/main/java/com/example/board/repository/ReplyRepository.java
+++ Board/src/main/java/com/example/board/repository/ReplyRepository.java
@@ -11,4 +11,6 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     @Query("select r from Reply r where r.board.bno = :bno")
     Page<Reply> listOfBoard(@Param("bno") Long bno, Pageable pageable);
+
+    void deleteByBoard_Bno(Long bno);
 }
