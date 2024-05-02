b6e32eaf45ed1f0c240fce19488ba15076343a19
ZzinB <mysteleee7@gmail.com>
2024-05-02 15:35:37 +0900

    Add: customUserDetailService

--- src/main/java/com/example/board/security/CustomUserDetailsService.java
+++ src/main/java/com/example/board/security/CustomUserDetailsService.java
@@ -1,7 +1,11 @@
 package com.example.board.security;
 
+import com.example.board.domain.Member;
+import com.example.board.repository.MemberRepository;
+import com.example.board.security.dto.MemberSecurityDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,24 +14,42 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
 
-    public CustomUserDetailsService(){
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("loadUserByUsername: " + username);
 
-        UserDetails userDetails = User.builder()
-                .username("user1")
-                .password(passwordEncoder.encode("1111")) //패스워드 인코딩 필요
-                .authorities("ROLE_USER")
-                .build();
-        return userDetails;
+        Optional<Member> result = memberRepository.getWithRoles(username);
+
+        //해당 아이디를 가진 사용자가 없다면
+        if(result.isEmpty()){
+        throw new UsernameNotFoundException("username not found !");
+        }
+
+        Member member = result.get();
+
+        MemberSecurityDTO memberSecurityDTO = new MemberSecurityDTO(
+                member.getMid(),
+                member.getMpw(),
+                member.getEmail(),
+                member.isDel(),
+                false,
+                member.getRoleSet().stream().map(memberRole -> new SimpleGrantedAuthority("ROLE_" + memberRole.name()))
+                        .collect(Collectors.toList())
+        );
+
+        log.info("memberSecurityDTO");
+        log.info(memberSecurityDTO);
+
+        return memberSecurityDTO;
     }
 }
