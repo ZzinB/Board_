acae1e0aa527b29c231bcea4a52cf1066d2a0583
ZzinB <mysteleee7@gmail.com>
2024-05-02 14:19:49 +0900

    Feat: spring security

--- src/main/java/com/example/board/config/CustomSecurityConfig.java
+++ src/main/java/com/example/board/config/CustomSecurityConfig.java
@@ -2,20 +2,35 @@ package com.example.board.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
-@RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     //모든 사용자가 모든 경로에 접근 가능
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
         log.info(("----------configure----------"));
+        http.formLogin();
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        log.info("----------web configure----------");
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
