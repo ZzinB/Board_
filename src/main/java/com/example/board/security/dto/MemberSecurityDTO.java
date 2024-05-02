f1e4801c47e23fe5580d2fa78679d723ab44ec45
ZzinB <mysteleee7@gmail.com>
2024-05-02 15:28:45 +0900

    Add: 회원 dto

---
+++ src/main/java/com/example/board/security/dto/MemberSecurityDTO.java
@@ -0,0 +1,29 @@
+package com.example.board.security.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@ToString
+public class MemberSecurityDTO extends User {
+    private String mid;
+    private String mpw;
+    private String email;
+    private boolean del;
+    private boolean social;
+
+    public MemberSecurityDTO(String username, String password, String email, boolean del, boolean social, Collection<? extends GrantedAuthority> authorities ) {
+        super(username, password, authorities);
+        this.mid = mid;
+        this.mpw = mpw;
+        this.email = email;
+        this.del = del;
+        this.social = social;
+    }
+}
