02a21942d515c80ad5abe7ee642e29365a03f13f
ZzinB <mysteleee7@gmail.com>
2024-05-02 13:44:30 +0900

    Comment: security 제외 접근

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,9 +2,11 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableJpaAuditing  //AuditingEntityListener 활성화
 public class BoardApplication {
 
