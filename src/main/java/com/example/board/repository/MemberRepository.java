0f13713639c58d0480b2b010492c33d8e3366a31
ZzinB <mysteleee7@gmail.com>
2024-05-03 12:37:13 +0900

    Feat: 패스워드 업데이트

--- src/main/java/com/example/board/repository/MemberRepository.java
+++ src/main/java/com/example/board/repository/MemberRepository.java
@@ -3,7 +3,10 @@ package com.example.board.repository;
 import com.example.board.domain.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,4 +17,9 @@ public interface MemberRepository extends JpaRepository<Member, String > {
 
     @EntityGraph(attributePaths = "roleSet")
     Optional<Member> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("update Member  m set m.mpw = :mpw where m.mid = :mid")
+    void updatePassword(@Param("mpw") String password, @Param("mid") String mid);
 }
